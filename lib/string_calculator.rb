dec4f45291f589e08b1871c1c8687dcd7fb23e42
Mehul Jain <mehuljain160@gmail.com>
2025-06-23 09:33:40 +0530

    refactor custom delimiter logic

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,11 +1,19 @@
 class StringCalculator
-  def add(numbers)
-    delimiter = /,|\n/
-    if numbers.start_with?("//")
-      parts = numbers.split("\n", 2)
+  def add(string_numbers)
+    numbers, delimiter = parse_input(string_numbers)
+    numbers.split(delimiter).map(&:to_i).sum
+  end
+
+  private
+
+  def parse_input(string_numbers)
+    if string_numbers.start_with?("//")
+      parts = string_numbers.split("\n", 2)
       delimiter = parts[0][2]
       numbers = parts[1]
+      [numbers, delimiter]
+    else
+      [string_numbers, /,|\n/]
     end
-    numbers.split(delimiter).map(&:to_i).sum
   end
 end
